fb233781dd46eae8d027dc5dce76f521aeb9def0
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-04-01 11:36:22 -0400

    created Dictionary.js and .css files.
    Created initial application layout components/styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
+import Dictionary from "./Dictionary.js";
 import logo from "./logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Candace Robinson</small>
+        </footer>
+      </div>
     </div>
   );
 }
