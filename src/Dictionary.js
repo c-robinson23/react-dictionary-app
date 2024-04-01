072549f59be1ddd0bca8707ab44ea47f56249720
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-04-01 13:04:19 -0400

    Update Dictionary.js file
    Added states/useState to keep track of the entered value that changes and display when necessary
    
    Added event.preventDefault to stop  certain automatic behaviors

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">Hello Dictionary</div>;
+  let [keyword, setKeyword] = useState(" ");
+
+  function handleKeywordChange(event) {
+    //console.log(event);
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for the ${keyword} definition...`);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
