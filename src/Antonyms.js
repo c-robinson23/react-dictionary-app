6824f8aff74be5a521e20da6d11e80aed8682b1e
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-04-04 12:07:27 -0400

    Updated the Antonyms component
    map method used to loop through props.antonyms values and display them one by one

--- src/Antonyms.js
+++ src/Antonyms.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Antonyms(props) {
   //console.log(props);
   if (props.antonyms) {
-    return props.antonyms;
+    return (
+      <ul className="Antonyms">
+        antonym(s):{" "}
+        {props.antonyms.map(function (antonym, index) {
+          return <li key={index}>{antonym}</li>;
+        })}
+      </ul>
+    );
   } else {
     return null;
   }
--- src/Meaning.js
+++ src/Meaning.js
@@ -6,13 +6,14 @@ export default function Meaning(props) {
   //console.log(props);
   return (
     <div className="Meaning">
-      <h4>({props.define.partOfSpeech})</h4>
+      <h5>({props.define.partOfSpeech})</h5>
       <div>
         definition: {props.define.definition}
         <br />
-        synonym(s): <Synonyms synonyms={props.define.synonyms} />
         <br />
-        antonym(s): <Antonyms antonyms={props.define.antonyms} />
+        <Synonyms synonyms={props.define.synonyms} />
+        <br />
+        <Antonyms antonyms={props.define.antonyms} />
         <br />
         <br />
       </div>
